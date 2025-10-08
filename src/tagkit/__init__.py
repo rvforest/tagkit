e66d8bb03c03eba44244975d3222b970721d48b3
Robert Forest <rvforest@gmail.com>
2025-10-07 21:35:28 -0400

    Add dynamic version lookup fallback

--- src/tagkit/__init__.py
+++ src/tagkit/__init__.py
@@ -1,7 +1,11 @@
-import importlib.metadata
+import importlib.metadata as _importlib_metadata
 
-__version__ = importlib.metadata.version(__name__)
+try:
+    __version__ = _importlib_metadata.version(__name__)
+except _importlib_metadata.PackageNotFoundError:
+    # editable/dev installs won't have package metadata
+    __version__ = "0+unknown"
 
 from tagkit.image import ExifImageCollection, ExifImage
 
-__all__ = ["ExifImageCollection", "ExifImage"]
+__all__ = ["ExifImageCollection", "ExifImage", "__version__"]
