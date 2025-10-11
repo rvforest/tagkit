5d0f99f7247c9782466cd012ec892d06cd3d858f
Robert Forest <rvforest@gmail.com>
2025-10-11 19:19:50 -0400

    Improve datetime tests

--- tests/image/test_exif_image.py
+++ tests/image/test_exif_image.py
@@ -7,7 +7,12 @@ from pathlib import Path
 
 from tagkit import ExifImage
 from tagkit.core.tag import ExifTag
-from tagkit.core.exceptions import InvalidTagId, InvalidTagName, TagNotFound
+from tagkit.core.exceptions import (
+    DateTimeError,
+    InvalidTagId,
+    InvalidTagName,
+    TagNotFound,
+)
 from tagkit.core.types import TagValue
 
 
@@ -317,76 +322,6 @@ class TestExifImageIntegration:
 class TestGetDatetime:
     """Tests for ExifImage.get_datetime() method."""
 
-    def test_get_datetime_from_datetime_original(self, test_images):
-        """Test getting datetime when DateTimeOriginal is present."""
-        image_path = test_images / "datetime_software.jpg"
-        exif = ExifImage(image_path)
-        dt = exif.get_datetime()
-        assert dt == datetime(2025, 5, 1, 14, 30, 0)
-
-    def test_get_datetime_specific_tag(self, test_images):
-        """Test getting a specific datetime tag."""
-        image_path = test_images / "datetime_software.jpg"
-        exif = ExifImage(image_path)
-        dt = exif.get_datetime(tag="DateTimeOriginal")
-        assert dt == datetime(2025, 5, 1, 14, 30, 0)
-
-    def test_get_datetime_nonexistent_tag(self, test_images):
-        """Test getting a nonexistent datetime tag returns None."""
-        image_path = test_images / "datetime_software.jpg"
-        exif = ExifImage(image_path)
-        dt = exif.get_datetime(tag="DateTime")
-        assert dt is None
-
-    def test_get_datetime_no_datetime_tags(self, test_images):
-        """Test getting datetime from image with no datetime tags."""
-        image_path = test_images / "minimal.jpg"
-        exif = ExifImage(image_path)
-        dt = exif.get_datetime()
-        assert dt is None
-
-
-class TestSetDatetime:
-    """Tests for ExifImage.set_datetime() method."""
-
-    def test_set_datetime_update_all(self, test_images):
-        """Test setting datetime with tags=None (updates all)."""
-        image_path = test_images / "datetime_software.jpg"
-        new_dt = datetime(2025, 6, 15, 10, 30, 0)
-
-        exif = ExifImage(image_path)
-        exif.set_datetime(new_dt, tags=None)
-        exif.save()
-
-        # Verify all datetime tags are updated
-        exif_verify = ExifImage(image_path)
-        datetimes = exif_verify.get_all_datetimes()
-        assert datetimes["DateTime"] == new_dt
-        assert datetimes["DateTimeOriginal"] == new_dt
-        assert datetimes["DateTimeDigitized"] == new_dt
-
-    def test_set_datetime_update_primary_only(self, test_images):
-        """Test setting datetime with specific tags list."""
-        image_path = test_images / "minimal.jpg"
-        new_dt = datetime(2025, 6, 15, 10, 30, 0)
-
-        exif = ExifImage(image_path)
-        exif.set_datetime(new_dt, tags=["DateTimeOriginal"])
-        exif.save()
-
-        # Verify only DateTimeOriginal is set
-        exif_verify = ExifImage(image_path)
-        dt = exif_verify.get_datetime(tag="DateTimeOriginal")
-        assert dt == new_dt
-
-        # DateTime should not be set
-        dt_other = exif_verify.get_datetime(tag="DateTime")
-        assert dt_other is None
-
-
-class TestGetAllDatetimes:
-    """Tests for ExifImage.get_all_datetimes() method."""
-
     def test_get_all_datetimes_single_tag(self, test_images):
         """Test getting all datetimes when only one is present."""
         image_path = test_images / "datetime_software.jpg"
@@ -420,6 +355,93 @@ class TestGetAllDatetimes:
         datetimes = exif.get_all_datetimes()
         assert len(datetimes) == 0
 
+    def test_get_datetime_specific_tag(self, test_images):
+        """Test getting a specific datetime tag."""
+        image_path = test_images / "datetime_software.jpg"
+        exif = ExifImage(image_path)
+        dt = exif.get_datetime(tag="DateTimeOriginal")
+        assert dt == datetime(2025, 5, 1, 14, 30, 0)
+
+    def test_get_datetime_missing_tag(self, test_images):
+        """Test getting a nonexistent datetime tag returns None."""
+        image_path = test_images / "datetime_software.jpg"
+        exif = ExifImage(image_path)
+        with pytest.raises(TagNotFound):
+            exif.get_datetime(tag="DateTime")
+
+    def test_get_datetime_invalid_tag_name(self, test_images):
+        """Test getting a datetime from an invalid tag name"""
+        image_path = test_images / "datetime_software.jpg"
+        exif = ExifImage(image_path)
+        with pytest.raises(InvalidTagName):
+            exif.get_datetime(tag="InvalidTag")
+
+    def test_get_datetime_from_non_datetime_tag(self, test_images):
+        """Test getting a datetime from a tag that isn't a datetime"""
+        image_path = test_images / "minimal.jpg"
+        exif = ExifImage(image_path)
+        with pytest.raises(DateTimeError):
+            exif.get_datetime(tag="Make")
+
+
+class TestSetDatetime:
+    """Tests for ExifImage.set_datetime() method."""
+
+    def test_set_datetime_update_all(self, test_images):
+        """Test setting datetime with tags=None (updates all)."""
+        image_path = test_images / "minimal.jpg"
+        new_dt = datetime(2025, 6, 15, 10, 30, 0)
+
+        exif = ExifImage(image_path)
+        exif.set_datetime(new_dt, tags=None)
+        datetimes = exif.get_all_datetimes()
+        assert datetimes["DateTime"] == new_dt
+        assert datetimes["DateTimeOriginal"] == new_dt
+        assert datetimes["DateTimeDigitized"] == new_dt
+
+    def test_set_datetime_single_new_tag(self, test_images):
+        """Test setting datetime with single tag that doesn't already exist."""
+        image_path = test_images / "minimal.jpg"
+        new_dt = datetime(2025, 6, 15, 10, 30, 0)
+
+        exif = ExifImage(image_path)
+        exif.set_datetime(new_dt, tags=["DateTime"])
+
+        datetimes = exif.get_all_datetimes()
+        assert datetimes["DateTime"] == new_dt
+        assert len(datetimes) == 1
+
+    def test_set_datetime_single_existing_tag(self, test_images):
+        """Test setting datetime with single tag that already exists."""
+        image_path = test_images / "datetime_software.jpg"
+        new_dt = datetime(2025, 6, 15, 10, 30, 0)
+
+        exif = ExifImage(image_path)
+        exif.set_datetime(new_dt, tags=["DateTimeOriginal"])
+
+        datetimes = exif.get_all_datetimes()
+        assert datetimes["DateTimeOriginal"] == new_dt
+        assert len(datetimes) == 1
+
+    def test_set_datetime_invalid_tag(self, test_images):
+        """Test setting datetime with an invalid tag raises error."""
+        image_path = test_images / "datetime_software.jpg"
+        new_dt = datetime(2025, 6, 15, 10, 30, 0)
+
+        exif = ExifImage(image_path)
+        with pytest.raises(InvalidTagName):
+            exif.set_datetime(new_dt, tags=["InvalidTag"])
+
+    def test_set_datetime_non_datetime_tag(self, test_images):
+        """Test setting datetime on a non-datetime tag raises error."""
+        image_path = test_images / "datetime_software.jpg"
+        new_dt = datetime(2025, 6, 15, 10, 30, 0)
+
+        exif = ExifImage(image_path)
+        with pytest.raises(Exception):
+            exif.set_datetime(new_dt, tags=["Flash"])
+            exif.save()
+
 
 class TestOffsetDatetime:
     """Tests for ExifImage.offset_datetime() method."""
@@ -434,10 +456,8 @@ class TestOffsetDatetime:
 
         delta = timedelta(hours=2, minutes=30)
         exif.offset_datetime(delta)
-        exif.save()
 
-        exif_verify = ExifImage(image_path)
-        new_dt = exif_verify.get_datetime()
+        new_dt = exif.get_datetime()
         assert new_dt == original_dt + delta
 
     def test_offset_datetime_negative(self, test_images):
@@ -446,14 +466,11 @@ class TestOffsetDatetime:
 
         exif = ExifImage(image_path)
         original_dt = exif.get_datetime()
-        assert original_dt is not None
 
         delta = timedelta(days=-1, hours=-3)
         exif.offset_datetime(delta)
-        exif.save()
 
-        exif_verify = ExifImage(image_path)
-        new_dt = exif_verify.get_datetime()
+        new_dt = exif.get_datetime()
         assert new_dt == original_dt + delta
 
     def test_offset_datetime_all_tags(self, test_images):
@@ -463,16 +480,12 @@ class TestOffsetDatetime:
 
         exif = ExifImage(image_path)
         exif.set_datetime(base_dt)
-        exif.save()
 
-        exif = ExifImage(image_path)
         delta = timedelta(hours=5)
         exif.offset_datetime(delta)
-        exif.save()
 
         # Verify all datetime tags are offset
-        exif_verify = ExifImage(image_path)
-        datetimes = exif_verify.get_all_datetimes()
+        datetimes = exif.get_all_datetimes()
         expected_dt = base_dt + delta
         assert datetimes["DateTime"] == expected_dt
         assert datetimes["DateTimeOriginal"] == expected_dt
@@ -486,17 +499,13 @@ class TestOffsetDatetime:
         # Set all tags first
         exif = ExifImage(image_path)
         exif.set_datetime(base_dt)
-        exif.save()
 
         # Offset only DateTimeOriginal
-        exif = ExifImage(image_path)
         delta = timedelta(hours=3)
         exif.offset_datetime(delta, tags=["DateTimeOriginal"])
-        exif.save()
 
         # Verify only DateTimeOriginal was offset
-        exif_verify = ExifImage(image_path)
-        datetimes = exif_verify.get_all_datetimes()
+        datetimes = exif.get_all_datetimes()
         assert datetimes["DateTimeOriginal"] == base_dt + delta
         assert datetimes["DateTime"] == base_dt
         assert datetimes["DateTimeDigitized"] == base_dt
@@ -514,11 +523,9 @@ class TestDatetimePrecedence:
         exif.set_datetime(datetime(2025, 6, 20, 10, 0, 0), tags=["DateTime"])
         exif.set_datetime(datetime(2025, 6, 10, 10, 0, 0), tags=["DateTimeOriginal"])
         exif.set_datetime(datetime(2025, 6, 15, 10, 0, 0), tags=["DateTimeDigitized"])
-        exif.save()
 
         # get_datetime should return DateTimeOriginal
-        exif_verify = ExifImage(image_path)
-        dt = exif_verify.get_datetime()
+        dt = exif.get_datetime()
         assert dt == datetime(2025, 6, 10, 10, 0, 0)
 
     def test_precedence_datetime_digitized_second(self, test_images):
@@ -549,6 +556,8 @@ class TestDatetimePrecedence:
         exif_verify = ExifImage(image_path)
         dt = exif_verify.get_datetime()
         assert dt == datetime(2025, 6, 20, 10, 0, 0)
+
+
 # Tests for read_tag and read_tags methods
 @pytest.mark.parametrize("file_type", [str, Path])
 def test_read_tag_by_name(test_images, file_type):
